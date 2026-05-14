9b8e747965369e9ffa7ae7efb3437219d188fba1
nypblounge <nypblounge@gmail.com>
2026-05-13 19:57:22 -0500

    Revert "Fix NPC respawn caps and add in-game NPC tracker"
    This reverts commit 916f67e87d49e633b0ed14423af8f996fedd45dd.

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDPGSnYIEN39jZZHOV4BF0xNQXAdiuhyEI",
+  authDomain: "quadrants-beta.firebaseapp.com",
+  databaseURL: "https://quadrants-beta-default-rtdb.firebaseio.com",
+  projectId: "quadrants-beta",
+  storageBucket: "quadrants-beta.firebasestorage.app",
+  messagingSenderId: "778411071125",
+  appId: "1:778411071125:web:7ba3b3de48fa9718e1ad9c"
+};
